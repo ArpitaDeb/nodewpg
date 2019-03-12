280ca525061d684dd588c24d1ae886ef00c25ff4
Arpita Deb <arpitadeb1990@gmail.com>
2019-03-12 13:45:14 -0600

    changes

---
+++ js/naa1.js
@@ -0,0 +1,41 @@
+var firstName = 'Dan';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a '+ age + ' year old '+ job + '. Is he married? ' + isMarried);
+
+age = 'twenty eight';
+
+var year, yearNel, yearArp;
+year = 2020;
+yearNel = year - 28;
+yearArp = year - 15;
+
+console.log(yearNel);
+
+var now = 2018;
+
+console.log( now + 2);
+
+var firstName = 'John';
+var age = 16;
+
+
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+console.log(ageJohn);
+
+CREATE TABLE GROCERYSHOP
+(
+    EmployeeID 'int',
+    VegetableName
+)
